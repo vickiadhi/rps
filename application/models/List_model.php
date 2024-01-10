f91b4082a11dab7e22d1ac86224f25f0a60e4e5c
vickiadhi <vickiadhip@gmail.com>
2024-01-11 00:35:51 +0700

    rps

--- application/models/List_model.php
+++ application/models/List_model.php
@@ -9,10 +9,22 @@ class List_model extends CI_Model
         return $this->db->query($query)->result_array();
     }
 
+    public function getListMateri()
+    {
+        $query = "SELECT db_materi.* FROM db_materi";
+        return $this->db->query($query)->result_array();
+    }
+
     public function getEdit($id_data)
     {
         $this->db->where('id_data', $id_data);
         return $this->db->get('db_add')->row_array();
+    }
+
+    public function getEditMateri($id_materi)
+    {
+        $this->db->where('id_materi', $id_materi);
+        return $this->db->get('db_materi')->row_array();
     }
     
 
@@ -23,23 +35,11 @@ class List_model extends CI_Model
         $this->db->delete('db_add');
     }
 
-    public function getListMateri()
-    {
-        $query = "SELECT db_materi.* FROM db_materi";
-        return $this->db->query($query)->result_array();
-    }
-
     public function deleteMateri($id_materi)
     {
         // Logika penghapusan berdasarkan $id_materi
         $this->db->where('id_materi', $id_materi);
         $this->db->delete('db_materi');
-    }
-
-    public function getEditMateri($id_materi)
-    {
-        $this->db->where('id_materi', $id_materi);
-        return $this->db->get('db_materi')->row_array();
     }
 
     public function getCreate()
